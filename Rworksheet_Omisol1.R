42a541805e2f1776bcdcb2164ff38de21764e6ad
Carl-Jason-Omisol <carljason.omisol@students.isatu.edu.ph>
2023-09-20 09:06:22 +0000

    commit

---
+++ Rworksheet_Omisol1.R
@@ -0,0 +1,60 @@
+#1.
+
+numberVector <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41,51, 35, 24, 33, 41)
+
+sort(numberVector)              
+
+
+seq(numberVector)
+
+assign(numberVector)
+
+min(numberVector)
+
+max(numberVector)
+
+c(numberVector)
+
+sort(numberVector)
+
+sum(numberVector)
+
+#2.
+
+#Find the reciprocal of the values for age.Write the R code and its output.
+recip_age<-1/numberVector
+recip_age
+
+
+
+#3.
+new_age<- c(75, 0 , 100)
+
+
+#4.
+
+Sort the values for age.Write the R code and its output.
+sort(numberVector)
+sort(recip_age)
+
+#5.
+
+Find the minimum and maximum value for age.Write the R code and its output.
+
+
+
+#7.
+
+Generates a new vector for data where you double every value of the data. | What happento the data?
+dob_data <-2*numberData 
+dob_data
+
+
+
+#8.
+sequence
+#int(1-100)
+seq(1,100)
+#numbers(20-60)
+seq(20,60)
+
